114f9742993d6babb3af098ed865bc6dd36611ea
Donald Hoelle <dhoelle@gmail.com>
2019-11-21 23:04:02 -0800

    bump to 0.4.1

--- Formula/redactr.rb
+++ Formula/redactr.rb
@@ -1,9 +1,9 @@
 class Redactr < Formula
   desc "Keep obscured secrets alongside plaintext"
   homepage ""
-  url "https://github.com/dhoelle/redactr/releases/download/v0.4.0/redactr_darwin_amd64.tar.gz"
-  version "0.4.0"
-  sha256 "2cc9b46367745a1218bd503dddae3ff0414f77d85d141136f07f47bde21f2f12"
+  url "https://github.com/dhoelle/redactr/releases/download/v0.4.1/redactr_darwin_amd64.tar.gz"
+  version "0.4.1"
+  sha256 "1449375e36497d0b30f134e6f4c70743d13d52827e71b8eb50a275dbd3ce17a1"
 
   def install
     bin.install "redactr"
